1742b73380b58c1318396d8337af71f2e2ed9b10
Alisha Sultan <123749748+AlishaSultan@users.noreply.github.com>
2023-08-08 22:24:21 +0500

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,74 @@
+function week(){
+
+    var day = parseInt(document.getElementById("day").value);
+
+    switch (day) {
+        case 1:
+            
+            alert("Today is Monday");
+            
+        break;
+
+        case 2:
+            
+            alert("Today is Tuesday");
+            
+        break;
+
+        case 3:
+            
+            alert("Today is Wednesday");
+            
+        break;
+
+        case 4:
+            
+            alert("Today is Thursday");
+            
+        break;
+
+        case 5:
+            
+            alert("Today is Friday");
+            
+        break;
+
+        case 6:
+            
+            alert("Today is Saturday");
+            
+        break;
+
+        case 7:
+            
+            alert("Today is Sunday");
+            
+        break;
+
+        default:
+             alert("Invalid day number");
+    }
+
+
+
+
+}
+
+function feedback() {
+
+    var a = prompt("What is your name? ")
+    var b = confirm("Do you like our website?");
+
+    if(a) {
+        alert("Hi!!!" + a);
+    }
+
+    if(b){
+        alert("Thanks for your feedback!");
+
+    }else{
+    
+        alert("Sorry, we'll improve our website");
+    }
+    
+}
