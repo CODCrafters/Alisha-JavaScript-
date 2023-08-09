998f82bfca368200917556b427f97d5d60d0674d
Alisha Sultan <123749748+AlishaSultan@users.noreply.github.com>
2023-08-09 23:09:28 +0500

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,11 @@
+
+    function on() {
+        document.getElementById("bulb");
+        bulb.src = "./assets/on.jpg";
+    }
+
+    function off() {
+        var bulb = document.getElementById("bulb");
+        bulb.src = "./assets/off.avif";
+    }
+
