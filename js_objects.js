5eac31f0585ada0c2e55350234ec50b728d233ba
Alisha Sultan <123749748+AlishaSultan@users.noreply.github.com>
2023-08-12 23:15:06 +0500

    Add files via upload

---
+++ js_objects.js
@@ -0,0 +1,76 @@
+// var a = {
+//     Name : "Alisha",
+//     Age: 20,
+//     Last_name: "Sultan",
+
+//     Students: ["Alisha" , "Seerat" , "Ansa" , "Anam"],
+
+//     living:{
+//         bedroom:1,
+//         dinning:2,
+//         kitchen:3,
+//     },
+
+//     details:function() {
+//         return this.Name + this.Last_name;
+//     },
+
+//     age:function() {
+//         return this.Age;
+//     }
+
+// };
+
+// document.write(a.details());
+// document.write(a.age());
+
+
+
+// var obj = new Object();
+
+// obj.firstname = "Alisha",
+// obj.lastname= "Sultan"
+
+// document.write(obj.firstname);
+
+
+//Array of objects:
+
+// var details = [
+//     {
+//         name: "Alisha",
+
+//         last_name:"Sultan",
+//         age:20,
+    
+    
+//     },
+
+// ];
+
+    // for(var i = 0; i<details.length; i++) {
+    //     document.write(details[i].name + details[i].last_name+ details[i].age )
+
+    // }
+
+
+//for in loop
+// var details = 
+//     {
+//         name: "Alisha",
+
+//         last_name:"Sultan",
+//         age:20,
+//     };
+
+//     for(var i in details) {
+//         document.write(details[i] + "<br>");
+//     }
+    
+    
+   
+
+
+
+
+
