62ab3ec0d930f744dffe46dcab3b57177048c2c9
Alisha Sultan <123749748+AlishaSultan@users.noreply.github.com>
2023-08-13 13:11:38 +0500

    Add files via upload

---
+++ date_methods.js
@@ -0,0 +1,20 @@
+// var now = new Date();
+
+// toDateString()
+// getDate()
+// getFullYear()
+// getMonth()
+// getDay()
+// getHours()
+// getMinutes()
+// getSeconds()
+// getTimezoneOffset()
+// getMilliseconds()
+// setDate()
+// setFullYear()
+// setHours()
+// setMilliseconds()
+// setMinutes()
+// setMonth()
+// setSeconds()
+
