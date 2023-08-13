8897014a704d12e877e2863b5f6b57443d46ea07
Alisha Sultan <123749748+AlishaSultan@users.noreply.github.com>
2023-08-13 12:56:52 +0500

    Add files via upload

---
+++ math_method.js
@@ -0,0 +1,41 @@
+var a = Math.ceil(45.7678);
+console.log('a:', a) // 46
+
+var a = Math.floor(45.7678);
+console.log('a:', a)// 45
+
+var a = Math.round(45.7678);
+console.log('a:', a);// 46
+
+var a = Math.trunc(45.90876);
+console.log('a:', a);// 45
+
+var a = Math.max(56 , 78, 100 , 450);
+console.log("Max:", a );// 450
+
+var a = Math.min(87, 12  , 4 , 6 , 1);
+console.log("Min", a ) ;//  1
+
+var a = Math.sqrt(64);
+console.log ("Square root of ", a);// 8
+
+var a = Math.cbrt(125);
+console.log ('Cube Root', a);// 3
+
+var a = Math.pow(4 , 8);
+console.log ( 'Pow ', a);// 256
+
+var a = Math.random();
+console.log ('Random number between [0-1]', a);// Random number between[0 - 1]
+
+var a = Math.abs(-125);
+console.log ('Absolute value' + a);// Absolute Value
+
+var a = Math.PI();
+console.log ('Value of PI is :'+ a);// Value Of Pi Is:
+
+
+
+
+
+
