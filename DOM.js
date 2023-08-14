4da500ba9be50d8cd72473e6d24c89ae4457f8cc
Alisha Sultan <123749748+AlishaSultan@users.noreply.github.com>
2023-08-14 18:43:41 +0500

    Add files via upload

--- DOM.js
+++ DOM.js
@@ -87,6 +87,289 @@ element = document.querySelector("list").classList; //array return krta hai
 document.querySelector("list").classList.remove("abc");
 
 
+//add Listener Method:
+
+document.getElementById("list").onclick = abc;
+
+function onclick() {
+    document.getElementsById("list").style.backgroundColor = "black";
+}
+//Event Listeners:
+
+document.getElementById("list").addEventListener("click" , functionname);  //don't use on
+
+//another method
+document.getElementById("list").addEventListener("click" , function(){
+        this.style.backgroundColor = "black";
+});
+
+//use capture---> agr true hai tu bd wala function pehly chalay ga or agr false hai tu sequence sy hi chalay ga
+
+//also use removeEventListener
+
+
+//classlist methods:
+
+document.getElementById("list").addEventListener("click" , function() {
+         this.classList.add("xyz");
+});
+
+document.getElementById("list").addEventListener("click" , function() {
+    this.classList.remove("xyz");
+});
+
+document.getElementById("list").classList.length;
+
+document.getElementById("list").classList.toggle("xyz");
+//get elements by class name and id:
+
+document.getElementById("list").classList.index(0); //zero index class return kr dein ga
+
+document.getElementById("list").classList.contains("xyz");  //return true if exist otherwise false
+
+
+//DOM Traversal Methods:
+
+// parentNode
+// ParentElement
+// Children
+// ChildNodes
+// firstChild
+// firstElementChild
+// lastChild
+// lastElementChild
+// nextElementSibling
+// previousElementSibling
+// previousSibling
+
+//childern ky through parent ko target krna
+document.getElementById("list").parentElement   //return parentelementof list
+document.getElementById("list").parentElement.style.backgroundColor = "black"; //apply css on parent element
+
+//parentNode---->kuch nah kuch return karein gay lekin parentElement agr parent exist ni krta tu null return karein ga
+
+
+//parent ky through children ko target krna
+document.getElementById("list").children;
+//return all child of list
+document.getElementById("list").children[0];
+//return first child of list
+
+document.getElementById("list").childNodes;
+//all node in list  //return with text , space , comment sb aa jata hai
+
+document.getElementById("list").firstElementChild;
+//first child of list
+
+document.getElementById("list").lastElementChild;
+//last child of list
+
+document.getElementById("list").firstChild;
+//first node of list  //return with text , space , comment sb aa jata hai
+
+document.getElementById("list").lastChild;
+//last node of list    //return with text , space , comment sb aa jata hai
+
+document.getElementById("list").nextElementSibling;
+//next sibling of list
+
+document.getElementById("list").previousElementSibling;
+//previos sibling of list
+
+document.getElementById("list").previousSibling;
+////return with text , space , comment sb aa jata hai
+
+document.getElementById("list").nextSibling;
+//return with text , space , comment sb aa jata hai
+
+
+//javascript sy koi tag , comment, text element create krna hai tu 
+
+var newelement = document.createElement("h2");
+///create new html element and return it to variable
+
+var newtext = document.createTextNode("Hello my name is alisha");
+/////create new textnode and return it to variable
+
+var element = document.createComment("hello my name is alisha");
+//////create new commet nd return it to variable
+
+
+//append: only append
+
+newelement.appendChild(newtext);
+///child append
+
+document.getElementById("test").appendChild(newelement);
+///parent append
+  
+var target = document.getElementById("test");
+target.insertBefore(newelement,childNodes[0]);
+///insert before first child
+
+
+//append + create
+
+var newelement = document.createElement("h2");
+
+var newText = document.createTextNode("This is just text");
+
+newelement.appendChild(newtext);
+
+var target = document.getElementById("test");
+target.insertAdjacentElement("afterbegin" , newelement);
+
+
+//insertAdjancentHTML
+
+var target = document.getElementById("test");
+
+var newElement = "<h2>this a Heading</h2>";
+
+target.insertAdjacentHTML("afterbegin" , newelement);
+
+
+
+
+//insertAdjacentText
+var target = document.getElementById("test");
+
+var newtext = "this a Heading";
+
+target.insertAdjacentText("afterbegin" , newtext);
+
+
+//replaceChild:
+
+var newElement = document.createElement("li");
+
+var newtext = document.createTextNode("Wow next Text");
+
+newElement.append(newtext);
+
+var target = document.getElementById("list");
+var oldchild= target.children[1]; //get all children of the list element
+
+target.replaceChild(newElement ,oldchild );
+
+//remove Child
+
+var target = document.getElementById("list");
+var oldchild= target.children[0]; //get all children of the list element
+
+target.removeChild(oldchild);
+
+
+//cloneNode:
+
+var target = document.getElementById("list1").children[0];
+var copyElement = target.cloneNode(true);
+
+document.getElementById("list2").appendChild(copyElement); 
+
+
+//contains method:
+
+
+var parentElement = document.getElementById("test");
+var target = document.getElementById("abc");
+
+var find = parentElement.contains(target);
+
+//hasAttribute
+
+var target = document.getElementById("test");
+
+var find = target.hasAttribute("class");
+//class name ka attribute exist karay ga tu true return karein ga warna false
+
+
+//hasChildNode
+
+var target = document.getElementById("test");
+
+var find = target.hasChildNodes();
+
+//agr child exist karay ga then return true warna false
+
+
+//isEqualNode()
+
+// same Node Type
+// same nodeName
+// same NodeValue
+// same childNodes
+//same attributes
+//same attribute values
+
+var target1 = document.getElementById("list1").firstElementChild;
+var target2 = document.getElementById("list3").firstElementChild;//both are li tag
+
+var equal = target1.isEqualNode(target2);
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
